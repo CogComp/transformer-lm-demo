cc8e6f59d5476c5b521e4d035adb45b82019e9b3
schen149 <schen149@illinois.edu>
2021-09-16 02:51:26 -0400

    bumping up transformers version; add support for different models

--- app/views.py
+++ app/views.py
@@ -6,7 +6,7 @@ from django.views.decorators.clickjacking import xframe_options_exempt
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
-from app.bert_lm import BERT_LM_predictions
+from app.bert_lm import BERT_LM_predictions, LIST_OF_MODEL_NAMES
 
 BLM = BERT_LM_predictions()
 
@@ -14,7 +14,8 @@ BLM = BERT_LM_predictions()
 @xframe_options_exempt
 def bert_demo(request):
     context = {
-        "alg": "perToken"
+        "alg": "perToken",
+        "model_list": LIST_OF_MODEL_NAMES
     }
 
     return render(request, 'bert.html', context)
@@ -27,7 +28,7 @@ def get_server_status(request):
 
 @csrf_exempt
 @xframe_options_exempt
-def bert_calculations(request, sent1, sent2, alg):
+def bert_calculations(request, sent1, sent2, alg, model_name):
     ns_label = None
     ns_label_neg = None
     ns_label_score = None
@@ -38,15 +39,15 @@ def bert_calculations(request, sent1, sent2, alg):
         print("there is no masked token in the second sentence")
     if alg == "perToken":
         print("Pertoken calculations")
-        predictedTokens, tokens = BLM.calculate_bert_masked_per_token(sent1, sent2)
+        predicted_tokens, tokens = BLM.calculate_bert_masked_per_token(sent1, sent2, model_name=model_name)
         print("Tokens: " + str(tokens))
     elif alg == "greedy":
-        predictedTokens, tokens = BLM.calculate_bert_masked_beam_search(sent1, sent2, beam_size=1)
+        predicted_tokens, tokens = BLM.calculate_bert_masked_beam_search(sent1, sent2, beam_size=1)
     elif alg == "beamSearch":
-        predictedTokens, tokens = BLM.calculate_bert_masked_beam_search(sent1, sent2, beam_size=3)
+        predicted_tokens, tokens = BLM.calculate_bert_masked_beam_search(sent1, sent2, beam_size=3)
     elif alg == "nextSentence":
         ns_label, ns_label_neg, ns_label_score, ns_label_neg_score = BLM.calculate_next_sentence_prediction(sent1, sent2)
-        predictedTokens, tokens = BLM.calculate_bert_masked_per_token(sent1, sent2)
+        predicted_tokens, tokens = BLM.calculate_bert_masked_per_token(sent1, sent2, model_name)
         if ns_label == 1:
             ns_label = "False"
             ns_label_neg = "True"
@@ -57,19 +58,19 @@ def bert_calculations(request, sent1, sent2, alg):
         ns_label_color = "green"
 
     table = []
-    random_value = random.choice(list(predictedTokens.values()))
+    random_value = random.choice(list(predicted_tokens.values()))
     num_rows = len(random_value)
     for rowId in list(range(0, num_rows)):
         row = []
         for tokenId, _ in enumerate(tokens):
-            if tokenId in predictedTokens:
-                row.append(predictedTokens[tokenId][rowId])
+            if tokenId in predicted_tokens:
+                row.append(predicted_tokens[tokenId][rowId])
             else:
                 row.append("")
         table.append(row)
 
     context = {
-        "predictedTokens": predictedTokens,
+        "predicted_tokens": predicted_tokens,
         "tokens": tokens,
         "table": table,
         "sent1": sent1,
@@ -79,6 +80,7 @@ def bert_calculations(request, sent1, sent2, alg):
         "nextSentence_neg": ns_label_neg,
         "nextSentence_score": ns_label_score,
         "nextSentence_neg_score": ns_label_neg_score,
-        "nextSentence_color": ns_label_color
+        "nextSentence_color": ns_label_color,
+        "model_list": LIST_OF_MODEL_NAMES
     }
     return render(request, 'bert.html', context)
